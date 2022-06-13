220c704d4ff201e0b171dfd1828f6982ebc905cb
fedeventi <sangreguardia@gmail.com>
2022-06-13 14:09:11 -0300

    Update AmmoKit.cs

--- Assets/Scripts/Extras/AmmoKit.cs
+++ Assets/Scripts/Extras/AmmoKit.cs
@@ -29,7 +29,7 @@ public class AmmoKit : MonoBehaviour
             if (player.weaponHolder.weaponsCollected.Count > 1)
             {
                 player.weaponHolder.weaponsCollected[(int)WeaponType.Pistol].GetComponent<GunPistol>().currentMaxAmmo += 14;
-                player.weaponHolder.weaponsCollected[(int)WeaponType.Pistol].GetComponent<GunPistol>().currentMaxAmmo += 6;
+                player.weaponHolder.weaponsCollected[(int)WeaponType.Shotgun].GetComponent<GunPistol>().currentMaxAmmo += 6;
             }
            
             Destroy(gameObject, 0.2f);
