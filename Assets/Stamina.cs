ffba7baa698d94f11b41865b373ec47a4d8b47dd
fedeventi <sangreguardia@gmail.com>
2023-07-31 12:26:29 -0300

    Update Stamina.cs

--- Assets/Stamina.cs
+++ Assets/Stamina.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Stamina : MonoBehaviour
 {
     PlayerModel player;
     public Image staminaBar, staminaBckgrnd;
     Color _BckgrndHSV;
     Color _BckgrndRGB;
+    float _breath;
     // Start is called before the first frame update
+    float map(float fromA, float toA, float fromB, float ToB, float value)
+    {
+        return fromB + (value - fromA) * (ToB - fromB) / (toA - fromA);
+    }
     void Start()
     {
         player = FindFirstObjectByType<PlayerModel>();
@@ -19,12 +25,15 @@ public class Stamina : MonoBehaviour
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        _breath += Time.deltaTime;
         if (!player) return;
 
-        staminaBckgrnd.color = Color.HSVToRGB()
+
+        staminaBckgrnd.color = Color.HSVToRGB(0.016f, 1, map(0, 1, 0.35f, 0.80f, (Mathf.Cos(_breath * 5) / 2) + 0.5f));
         if (player.isRunning)
             staminaBar.fillAmount = Mathf.Lerp(0, 1, player.estamina / 100);
 
