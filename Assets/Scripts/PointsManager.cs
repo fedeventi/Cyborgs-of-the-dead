c6567f81d74fcf047fb27e6523c9f8e78099deb4
fedeventi <sangreguardia@gmail.com>
2023-08-08 16:17:33 -0300

    Update PointsManager.cs

--- Assets/Scripts/PointsManager.cs
+++ Assets/Scripts/PointsManager.cs
@@ -8,24 +8,24 @@ using UnityEngine.SceneManagement;
 
 public class PointsManager : MonoBehaviour
 {
-    int Deaths;
-    int points;
-    int[] kills= new int[3];
-    int totalKills;
-    int[] pointsPerEnemyType= new int[3] {50,120,200};
-    public float finalCombo;
-    public float combo;
-    float time; 
-    float[] comboLimits = new float[5] { 100, 250, 400, 600, 800};
-    float[] comboDecreasement = new float[5] { 5, 8, 15, 25, 50 };
-    float[] comboMultiplier = new float[5] { 1, 1.3f, 1.5f, 1.8f, 2 };
-    public float comboTemp;
-    public int currentComboInstance;
+    int Deaths; //tus muertes   
+    int points; //puntos acumulados 
+    int[] kills = new int[3]; // muertes de zombies de cada tipo 
+    int totalKills; //total de muertes de zombies 
+    int[] pointsPerEnemyType = new int[3] { 50, 120, 200 }; //puntos que da cada tipo de enemigo 
+    float finalCombo;
+    float combo; //acumulacion de combo para subir el bonificador
+    float time;
+    float[] comboLimits = new float[5] { 100, 250, 400, 600, 800 }; //limite para cada etapa de bonificacion
+    float[] comboDecreasement = new float[5] { 15, 23, 30, 45, 60 };  //velocidad en la que decae el combo en cada etapa 
+    float[] comboMultiplier = new float[5] { 1, 1.3f, 1.5f, 1.8f, 2 }; //multiplicador en la cantidad de puntos que se consigue en cada etapa de combo
+    float comboTemp;
+    int currentComboInstance;
     public static PointsManager instance;
     public Slider slider;
     bool isFinished;
     public GameObject canvas;
-    public Text DeathText, killText, pointText, timeText, x,xshadow, multiplier,multiplierShadow;
+    public Text DeathText, killText, pointText, timeText, multiplier, multiplierShadow;
     bool show;
     // Start is called before the first frame update
     void Start()
@@ -46,13 +46,13 @@ public class PointsManager : MonoBehaviour
 
             time += Time.deltaTime;
             ChangeIndex();
-            if ( combo > 0)
+            if (combo > 0)
             {
-                combo -= Time.deltaTime*comboDecreasement[currentComboInstance];
-                if(multiplier && multiplierShadow)
+                combo -= Time.deltaTime * comboDecreasement[currentComboInstance];
+                if (multiplier && multiplierShadow)
                 {
-                    multiplier.text=comboMultiplier[currentComboInstance].ToString();
-                    multiplierShadow.text=comboMultiplier[currentComboInstance].ToString();
+                    multiplier.text = comboMultiplier[currentComboInstance].ToString();
+                    multiplierShadow.text = comboMultiplier[currentComboInstance].ToString();
                 }
             }
             else
@@ -63,20 +63,15 @@ public class PointsManager : MonoBehaviour
                     multiplierShadow.text = "";
                 }
             }
-            if(x && xshadow)
-            {
-                x.gameObject.SetActive(combo > 0);
-                xshadow.gameObject.SetActive(combo > 0);
 
-            }
-            
-        
-            comboTemp = ExtensionMethods.Remap(combo, currentComboInstance > 0 ? comboLimits[currentComboInstance - 1] :0, comboLimits[currentComboInstance], 0, 1);
-            if(slider)
+
+
+            comboTemp = ExtensionMethods.Remap(combo, currentComboInstance > 0 ? comboLimits[currentComboInstance - 1] : 0, comboLimits[currentComboInstance], 0, 1);
+            if (slider)
                 slider.value = comboTemp;
 
         }
-        if(SceneManager.GetActiveScene().name== "end level cinematic" && !show)
+        if (SceneManager.GetActiveScene().name == "end level cinematic" && !show)
         {
             show = true;
             StartCoroutine(WaitToFinish());
@@ -97,21 +92,21 @@ public class PointsManager : MonoBehaviour
     }
     public void AddCombo(int damage)
     {
-        finalCombo += damage*comboMultiplier[currentComboInstance];
-        if(combo<comboLimits[comboLimits.Length-1])
+        finalCombo += damage * comboMultiplier[currentComboInstance];
+        if (combo < comboLimits[comboLimits.Length - 1])
             combo += damage;
-        
+
     }
     public void ChangeIndex()
     {
-        if (combo > comboLimits[currentComboInstance] && currentComboInstance<comboLimits.Length-1)
+        if (combo > comboLimits[currentComboInstance] && currentComboInstance < comboLimits.Length - 1)
             currentComboInstance++;
         else if (currentComboInstance > 0)
         {
-            if(combo < comboLimits[currentComboInstance - 1])
+            if (combo < comboLimits[currentComboInstance - 1])
             {
                 Debug.Log(combo + " es menor a " + comboLimits[currentComboInstance - 1]);
-                
+
                 currentComboInstance--;
 
             }
