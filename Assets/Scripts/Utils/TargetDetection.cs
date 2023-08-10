1856e3d93a6aefb36c46a92e1c530fd815393a3a
fedeventi <sangreguardia@gmail.com>
2023-08-10 11:57:07 -0300

    Update TargetDetection.cs

--- Assets/Scripts/Utils/TargetDetection.cs
+++ Assets/Scripts/Utils/TargetDetection.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetDetection: MonoBehaviour
+public class TargetDetection : MonoBehaviour
 {
-   
+
     int debugIndex;
     [SerializeField]
     LayerMask walls;
     [Range(5, 25)]
-    public int amount=9;
+    public int amount = 9;
     bool[] debugIndexBoolColliding = new bool[25];
     public bool[] DebugIndexBoolColliding { get => debugIndexBoolColliding; set => debugIndexBoolColliding = value; }
     public bool debugBool;
@@ -22,7 +22,9 @@ public class TargetDetection: MonoBehaviour
     [Range(1, 1000)]
     float obstacleDetection;
     [SerializeField]
-
+    float _timeFromLastChange;
+    Vector3 _lastPositionCheck;
+    bool _decreaseAvoidance;
     public Transform MyClosestObstacle()
     {
 
@@ -61,8 +63,8 @@ public class TargetDetection: MonoBehaviour
             if (!Physics.Raycast(transform.position + new Vector3(0, verticalOffset, 0), (targetDir + transform.position) - transform.position, _obstacleAvoidance, walls))
             {
 
-                
-                    
+
+
 
                 possiblePoints[positionIndex] = transform.position + targetDir.normalized * _obstacleAvoidance;
                 distanceAux = Vector3.Distance(transform.position + targetDir.normalized * _obstacleAvoidance,
@@ -94,7 +96,7 @@ public class TargetDetection: MonoBehaviour
             else
             {
                 debugIndexBoolColliding[i] = true;
-                
+
             }
             positionIndex++;
         }
@@ -103,14 +105,41 @@ public class TargetDetection: MonoBehaviour
 
         return possiblePoints[targetIndex];
     }
-   
+    public void DidMyPositionChange(float seconds)
+    {
+        if (_lastPositionCheck == Vector3.zero)
+            _lastPositionCheck = transform.position;
+
+        if (Vector3.Distance(_lastPositionCheck, transform.position) <= 25)
+        {
+            _timeFromLastChange += Time.deltaTime;
+            if (_timeFromLastChange > seconds)
+            {
+                _obstacleAvoidance += _decreaseAvoidance ? -Time.deltaTime * 500 : Time.deltaTime * 500;
+                if (_obstacleAvoidance >= 1000)
+                    _decreaseAvoidance = true;
+                else if (_obstacleAvoidance <= 0)
+                    _decreaseAvoidance = false;
+
+
+            }
+        }
+        else
+        {
+            _lastPositionCheck = transform.position;
+            _timeFromLastChange = 0;
+        }
+
+
+    }
+
     private void OnDrawGizmos()
     {
 
 
 
         Gizmos.DrawWireSphere(transform.position, obstacleDetection);
-       
+
         for (int i = 0; i < amount; i++)
         {
             Gizmos.color = Color.yellow;
@@ -122,24 +151,24 @@ public class TargetDetection: MonoBehaviour
 
 
                 Gizmos.DrawRay(transform.position + new Vector3(0, verticalOffset, 0),
-                    ((Quaternion.AngleAxis((180f/(amount-1))*i,transform.up)*transform.right*-1+ transform.position) - transform.position) * obstacleDetection);
+                    ((Quaternion.AngleAxis((180f / (amount - 1)) * i, transform.up) * transform.right * -1 + transform.position) - transform.position) * obstacleDetection);
 
 
                 if (debugIndex == i)
                 {
                     Gizmos.color = Color.blue;
                     Gizmos.DrawWireSphere((transform.position + new Vector3(0, verticalOffset, 0) +
-                                  ((Quaternion.AngleAxis((180f / (amount - 1)) * i, transform.up) * 
-                                  transform.right * -1 + transform.position) - transform.position)* 
+                                  ((Quaternion.AngleAxis((180f / (amount - 1)) * i, transform.up) *
+                                  transform.right * -1 + transform.position) - transform.position) *
                                   obstacleDetection), 5f);
                 }
 
             }
         }
-        
+
         if (debugBool)
         {
-           
+
 
         }
 
