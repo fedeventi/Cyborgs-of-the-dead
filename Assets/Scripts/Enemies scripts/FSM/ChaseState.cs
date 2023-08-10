411d4695b32ab3f99586c0326826d0e852a0561f
fedeventi <sangreguardia@gmail.com>
2023-08-10 11:57:10 -0300

    Update ChaseState.cs

--- Assets/Scripts/Enemies scripts/FSM/ChaseState.cs
+++ Assets/Scripts/Enemies scripts/FSM/ChaseState.cs
@@ -7,6 +7,7 @@ public class ChaseState<T> : State<T>
     BaseEnemy _baseEnemy;
     EnemyView enemyView;
     float _speed;
+    float rotationTime = 0;
     public ChaseState(BaseEnemy enemy, EnemyView view)
     {
         _baseEnemy = enemy;
@@ -14,29 +15,31 @@ public class ChaseState<T> : State<T>
     }
     public override void Execute()
     {
+
         CheckDistanceWithTarget();
         if (!_baseEnemy.isDamage)
         {
             FollowPlayer();
-            
+            _baseEnemy.targetDetection.DidMyPositionChange(0.7f);
         }
+
     }
 
     public void FollowPlayer()
     {
-        _speed = _baseEnemy.speed*5;
+        _speed = _baseEnemy.speed * 5;
         enemyView.ChasingAnimation();
         var target = _baseEnemy.player.transform.position;
         target.y = _baseEnemy.transform.position.y;
 
-        
-        Vector3 dir = target - _baseEnemy.transform.position;
+
+        Vector3 dir;
 
         if (_baseEnemy.targetDetection.MyClosestObstacle())
         {
 
 
-            dir = (_baseEnemy.targetDetection.MyClosestPointToTarget(target) - _baseEnemy.transform.position);
+            dir = _baseEnemy.targetDetection.MyClosestPointToTarget(target) - _baseEnemy.transform.position;
 
         }
         else
@@ -45,15 +48,15 @@ public class ChaseState<T> : State<T>
 
 
         }
-        var rot = Quaternion.LookRotation(dir);
-        _baseEnemy.transform.rotation = Quaternion.Slerp(_baseEnemy.transform.rotation, rot, Time.deltaTime*3).normalized;
-        _baseEnemy.transform.position += _baseEnemy.transform.forward * _speed*Time.deltaTime;
+        var rot = Quaternion.LookRotation(dir.normalized);
+        _baseEnemy.transform.rotation = Quaternion.Slerp(_baseEnemy.transform.rotation, rot, Time.deltaTime);
+        _baseEnemy.rb.velocity += _baseEnemy.transform.forward * _speed * Time.deltaTime;
 
     }
     public void CheckDistanceWithTarget()
     {
         var distance = Vector3.Distance(_baseEnemy.player.transform.position, _baseEnemy.transform.position);
-       // if (distance > _baseEnemy.viewDistance) _baseEnemy.Transition("Patrol");
+        // if (distance > _baseEnemy.viewDistance) _baseEnemy.Transition("Patrol");
 
         if (_baseEnemy.InRangeToAttack())
         {
@@ -64,5 +67,6 @@ public class ChaseState<T> : State<T>
     public override void Sleep()
     {
         base.Sleep();
+
     }
 }
