09196a26f9ae2164dd9d98b1314de1b625b55635
fedeventi <sangreguardia@gmail.com>
2023-08-10 17:30:25 -0300

    Update ChaseState.cs

--- Assets/Scripts/Enemies scripts/FSM/ChaseState.cs
+++ Assets/Scripts/Enemies scripts/FSM/ChaseState.cs
@@ -51,7 +51,7 @@ public class ChaseState<T> : State<T>
         {
 
             dir = _baseEnemy.targetDetection.MyClosestPointToTarget(target) - _baseEnemy.transform.position;
-            // _rotationTime = 0;
+            _rotationTime = 0;
 
         }
         else
@@ -61,10 +61,9 @@ public class ChaseState<T> : State<T>
 
         }
         _rotationTime += Time.deltaTime;
-        var rot = Quaternion.LookRotation(dir.normalized);
-        _baseEnemy.transform.rotation = Quaternion.Slerp(_baseEnemy.transform.rotation, rot, _rotationTime);
+        _baseEnemy.transform.forward = Vector3.Lerp(_baseEnemy.transform.forward, dir, _rotationTime);
         _baseEnemy.rb.velocity += _baseEnemy.transform.forward * _speed * Time.deltaTime;
-
+        if (!_baseEnemy.LookingPlayer()) _baseEnemy.Transition("Search");
     }
 
     public void CheckDistanceWithTarget()
@@ -110,33 +109,39 @@ public class SearchState<T> : State<T>
     public override void Execute()
     {
 
-        GoToPlayer();
+        // GoToPlayer();
+        _baseEnemy.targetDetection.DidMyPositionChange(0.7f);
     }
     public void GoToPlayer()
     {
-        _speed = _baseEnemy.speed * 5;
+        _speed = _baseEnemy.speed * 10;
         // _speed = 500;
         enemyView.ChasingAnimation();
 
         waypoints = _path.corners;
-        if (Vector3.Distance(_baseEnemy.transform.position, waypoints[_currentWaypoint]) >= 50)
-        {
-            _rotationTime += Time.deltaTime;
-            var dir = waypoints[_currentWaypoint] - _baseEnemy.transform.position;
-            var rot = Quaternion.LookRotation(dir.normalized);
-            _baseEnemy.transform.rotation = Quaternion.Slerp(_baseEnemy.transform.rotation, rot, _rotationTime);
-            _baseEnemy.rb.velocity += _baseEnemy.transform.forward * _speed * Time.deltaTime;
+        if (_currentWaypoint < waypoints.Length - 1)
+            if (Vector3.Distance(_baseEnemy.transform.position, waypoints[_currentWaypoint]) >= 100)
+            {
+                _rotationTime += Time.deltaTime;
+                Vector3 dir;
+                if (_baseEnemy.targetDetection.MyClosestObstacle())
+                    dir = _baseEnemy.targetDetection.MyClosestPointToTarget(waypoints[_currentWaypoint]) - _baseEnemy.transform.position;
+                else
+                    dir = waypoints[_currentWaypoint] - _baseEnemy.transform.position;
 
-        }
-        else
-        {
+                _baseEnemy.transform.forward = Vector3.Lerp(_baseEnemy.transform.forward, dir, Time.deltaTime);
+                _baseEnemy.rb.velocity += _baseEnemy.transform.forward * _speed * Time.deltaTime;
 
-            if (_currentWaypoint < waypoints.Length - 1)
-                _currentWaypoint++;
-            if (playerHasMove)
-                RecalculatePath();
+            }
+            else
+            {
+                _rotationTime = 0;
+                if (_currentWaypoint < waypoints.Length - 1)
+                    _currentWaypoint++;
+                if (playerHasMove)
+                    RecalculatePath();
 
-        }
+            }
         Debug.Log($"waypoint:{_currentWaypoint}:{waypoints.Length}");
         // if (_baseEnemy.LookingPlayer()) _baseEnemy.Transition("Chase");
     }
