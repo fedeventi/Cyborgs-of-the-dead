ad1c97c40ddc9fc2ed05138f2ea9292aea9c495c
fedeventi <sangreguardia@gmail.com>
2023-08-11 15:15:49 -0300

    Update AttackState.cs

--- Assets/Scripts/Enemies scripts/FSM/AttackState.cs
+++ Assets/Scripts/Enemies scripts/FSM/AttackState.cs
@@ -10,8 +10,8 @@ public class AttackState<T> : State<T>
     Action _action;
     float _attackCadence;
     float effectCadence;
-    float effectCadenceMax=3;
-    
+    float effectCadenceMax = 3;
+
     public AttackState(BaseEnemy enemy, EnemyView view)
     {
         baseEnemy = enemy;
@@ -32,10 +32,10 @@ public class AttackState<T> : State<T>
     public override void Execute()
     {
 
-        
+
         if (!baseEnemy.isDamage)
         {
-            
+
             AimToTarget();
             Attack();
             ExecuteSecondaryEffect();
@@ -51,7 +51,7 @@ public class AttackState<T> : State<T>
         if (_attackCadence > 1.5f)
         {
             if (baseEnemy.InRangeToAttack())
-                    enemyView.AttackAnimation();    
+                enemyView.AttackAnimation();
             else
                 baseEnemy.Transition("Chase");
 
@@ -64,8 +64,9 @@ public class AttackState<T> : State<T>
         var target = baseEnemy.player.transform.position;
         target.y = baseEnemy.transform.position.y;
         Vector3 dir = target - baseEnemy.transform.position;
-        var rot = Quaternion.LookRotation(dir);
-        baseEnemy.transform.rotation = Quaternion.Slerp(baseEnemy.transform.rotation, rot, Time.deltaTime * 2).normalized;
+
+        var direction = Vector3.RotateTowards(baseEnemy.transform.forward, dir, 3 * Time.deltaTime, 0);
+        baseEnemy.transform.rotation = Quaternion.LookRotation(dir);
     }
     public void ExecuteSecondaryEffect()
     {
@@ -80,8 +81,8 @@ public class AttackState<T> : State<T>
     }
     public override void Sleep()
     {
-        
-        
+
+
     }
 
 }
